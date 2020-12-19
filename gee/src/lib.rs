df4049e4c9fa352b167b74be7b8dc19b79f48e17
Jean Pierre Dudey <me@jeandudey.tech>
2020-12-19 16:58:20 +0100

    Initial commit
    Signed-off-by: Jean Pierre Dudey <me@jeandudey.tech>

---
+++ gee/src/lib.rs
@@ -0,0 +1,7 @@
+pub use ffi;
+
+#[allow(unused_doc_comments)]
+#[allow(non_upper_case_globals)]
+#[allow(unused_imports)]
+mod auto;
+pub use self::auto::*;
---
+++ granite/src/lib.rs
@@ -0,0 +1,6 @@
+mod auto;
+pub use self::auto::*;
+
+pub mod prelude {
+    pub use super::{ApplicationExt, WidgetsWelcomeExt};
+}
